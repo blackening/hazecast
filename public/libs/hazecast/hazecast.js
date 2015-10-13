2701fa764245bfe8c8cead2f4bc11baf82c2a299
BlacKeNinG <blackening@gmail.com>
2015-10-13 13:59:39 +0800

    Exposed earth.js functions, getWindVelocity.

---
+++ public/libs/hazecast/hazecast.js
@@ -0,0 +1,10 @@
+function getWindVelocity(coord){
+  coord = coord || [];
+
+  var grids = gridAgent.value(),
+    field = fieldAgent.value(),
+    λ = coord[0],
+    φ = coord[1];
+  var wind = grids.primaryGrid.interpolate(λ, φ);
+  return wind; //Format [vx, vy, |v|]
+}
