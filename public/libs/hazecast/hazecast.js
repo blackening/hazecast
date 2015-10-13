0bc751bde6912962341c467078843cfb89b24dc6
BlacKeNinG <blackening@gmail.com>
2015-10-13 15:13:54 +0800

    D3 Renderer for hazecast.

--- public/libs/hazecast/hazecast.js
+++ public/libs/hazecast/hazecast.js
@@ -13,3 +13,39 @@ function getWindVelocity(coord){
 function coordToScreen(coord){
 	return globe.projection(coord);
 }
+
+var bottomLeft = [95, -10];
+var topRight = [121 , 6];
+
+var HazeGrid = _.map(_.range(bottomLeft[0], topRight[0], 0.1), function(x){
+	return _.map(_.range(bottomLeft[1], topRight[1], 0.1), function(y){
+		return {
+			x: x,
+			y: y,
+			mass: 0.1
+		};
+	});
+});
+
+//HazeGrid[long][lat] -> { x,y,mass }
+
+
+var HazePoints = _.flatten(HazeGrid);
+
+function render(){
+	var gridPoints = d3.select("#foreground").selectAll("circle")
+	.data(HazePoints);
+	gridPoints.enter().append("circle")
+	gridPoints.attr("cx", function(d,i,r){
+		var pt = coordToScreen([d.x, d.y]);
+		return pt[0];
+	})
+	gridPoints.attr("cy", function(d,i,r){
+		var pt = coordToScreen([d.x, d.y]);
+		return pt[1];
+	})
+	gridPoints.attr('r', function(d,i,r){
+		return d.mass*10;
+	})
+	gridPoints.attr('fill', 'red');
+}
